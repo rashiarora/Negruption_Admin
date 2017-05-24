75b74d6fe438cac887e5a0b4ee0c8362f0651510
rashiarora <rashiarora50@gmail.com>
2017-05-24 19:35:15 +0530

    Initial commit

---
+++ app/src/main/java/com/codeogic/negruption_admin/Util.java
@@ -0,0 +1,19 @@
+package com.codeogic.negruption_admin;
+
+/**
+ * Created by Admin on 24-05-2017.
+ */
+
+public class Util {
+    public static final String PREFS_NAME="negruption";
+    public static final String PREFS_KEYUSERID = "keyUserId";
+    public static final String PREFS_KEYUSERNAME="keyUsername";
+    public static final String PREFS_KEYPASSWORD="keyPassword";
+
+    public static final String INSERT_ADMIN = "http://codeogic.esy.es/negruption/insert_admin.php";
+
+    public static final String LOGIN_ADMIN = "http://codeogic.esy.es/negruption/login_admin.php";
+
+    public static final String UPDATE_VIEWS = "http://codeogic.esy.es/negruption/update_views.php";
+
+}
